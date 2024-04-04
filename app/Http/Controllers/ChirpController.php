30fd3093f0d114fa7786b520a416311166002c91
chamaxx9zp <chamakapiyumal@gmail.com>
2024-04-04 10:06:44 +0530

    saving the chirp to database

--- app/Http/Controllers/ChirpController.php
+++ app/Http/Controllers/ChirpController.php
@@ -6,6 +6,7 @@ namespace App\Http\Controllers;
 use App\Models\Chirp;
 use Illuminate\View\View;
 use Illuminate\Http\Request;
+use Illuminate\Http\RedirectResponse;
 
 class ChirpController extends Controller
 {
@@ -28,9 +29,15 @@ class ChirpController extends Controller
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(Request $request): RedirectResponse
     {
-        //
+        $validated = $request->validate([
+            'message' => 'required|string|max:255',
+        ]);
+ 
+        $request->user()->chirps()->create($validated);
+ 
+        return redirect(route('chirps.index'));
     }
 
     /**
