5d198b5672d4cfa62fd1030cc01c58f130c862c0
chamaxx9zp <chamakapiyumal@gmail.com>
2024-04-04 10:38:28 +0530

    retrive the saved chirps to index view

--- app/Models/Chirp.php
+++ app/Models/Chirp.php
@@ -2,8 +2,9 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Chirp extends Model
 {
@@ -12,4 +13,9 @@ class Chirp extends Model
     protected $fillable = [
         'message',
     ];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
 }
