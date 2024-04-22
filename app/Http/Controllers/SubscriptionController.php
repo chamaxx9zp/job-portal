281b3dbbcaca1be6c669cb440fade1ae35fd9011
chamaxx9zp <chamakapiyumal@gmail.com>
2024-04-22 11:31:42 +0530

    prohibit access to payment for job seekers

--- app/Http/Controllers/SubscriptionController.php
+++ app/Http/Controllers/SubscriptionController.php
@@ -3,9 +3,15 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Http\Middleware\isEmployer;
 
 class SubscriptionController extends Controller
-{
+{   
+    public function __construct()
+    {
+        $this->middleware(['auth', isEmployer::class]);
+    }
+
     public function subscribe()
     {
         return view('subscription.index');
